70b8230def688c679ac814a5127e841abee043c2
Michał Więcek <wiecek1873@gmail.com>
2022-04-11 17:53:22 +0200

    Added script to create mesh from code

--- Assets/Scripts/MeshEditor.cs
+++ Assets/Scripts/MeshEditor.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 
 public class MeshEditor : MonoBehaviour
 {
-	[SerializeField] private Mesh m_defaultMesh;
+	[SerializeField] private MeshFromCode m_meshFromCode;
 	[SerializeField] private MeshFilter m_meshFilter;
 	[SerializeField] private MeshRenderer m_meshRenderer;
 
+	[Header("Mesh properties")]
+	[SerializeField] private Vector3[] m_vertices;
+	[SerializeField] private int[] m_triangles;
+	[SerializeField] private Vector3[] m_normals;
+
+	[SerializeField] private bool m_recalculateNormals;
+	[SerializeField] private bool m_recalculateBounds;
+
+	private Mesh m_defaultMesh;
+	private Mesh m_currentMesh;
+
 	private void Start()
 	{
-		Mesh newMesh = new Mesh();
-		newMesh.vertices = m_defaultMesh.vertices;
-		newMesh.triangles = m_defaultMesh.triangles;
-		newMesh.normals = m_defaultMesh.normals;
+		m_defaultMesh = m_meshFromCode.GeneratedMesh;
 
-		m_meshFilter.mesh = newMesh;
+		m_currentMesh = new Mesh();
+		m_currentMesh.vertices = m_defaultMesh.vertices;
+		m_currentMesh.triangles = m_defaultMesh.triangles;
+		m_currentMesh.normals = m_defaultMesh.normals;
+
+		m_vertices = m_currentMesh.vertices;
+		m_triangles = m_currentMesh.triangles;
+		m_normals = m_currentMesh.normals;
+
+		m_meshFilter.mesh = m_currentMesh;
 	}
 
+	private void Update()
+	{
+		m_currentMesh.vertices = m_vertices;
+		m_currentMesh.triangles = m_triangles;
+
+		if(m_recalculateNormals)
+		m_currentMesh.RecalculateNormals();
+		if(m_recalculateBounds)
+		m_currentMesh.RecalculateBounds();
+	}
 }
---
+++ Assets/Scripts/MeshFromCode.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshFromCode : MonoBehaviour
+{
+	public Mesh GeneratedMesh;
+
+	[SerializeField] private Vector3[] m_vertices;
+	[SerializeField] private int[] m_triangles;
+
+	private void Awake()
+	{
+		GeneratedMesh = new Mesh();
+		GeneratedMesh.vertices = m_vertices;
+		GeneratedMesh.triangles = m_triangles;
+
+		Vector2[] uvs = new Vector2[m_vertices.Length];
+
+		for (int i = 0; i < uvs.Length; i++)
+		{
+			uvs[i] = new Vector2(m_vertices[i].x, m_vertices[i].z);
+		}
+
+		GeneratedMesh.uv = uvs;
+		GeneratedMesh.RecalculateNormals();
+	}
+}
